f1ea393e999fc14c12450ab200ab2606b59eff00
Alex Binkley <alex.binkley@gmail.com>
2013-08-10 11:51:41 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FantasyCycling::Application.config.secret_token = 'a5ed57e074ec968e62470e4c4790f006e0be32035d3f8e274ccb52e13d2ba0c37c5a67f8bcb643655d37e9736ef971e6079b9139d9efbce3a8b4dba1308bd8ed'
